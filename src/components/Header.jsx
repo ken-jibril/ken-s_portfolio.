7385a7ffb773abe801d200888d0c5a52d3d33354
ken kagai <jibken80@gmail.com>
2025-09-02 12:16:14 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="#home">Home</a>
+                <a href="#work">My Work</a>
+                <a href="#contact">Contacts</a>
+            </nav>
+        </header>
+    )
+}
