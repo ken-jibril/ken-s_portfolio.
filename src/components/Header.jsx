eed2a80880fa8eab0dc8fdea2ae9518b1f1f739f
ken kagai <jibken80@gmail.com>
2025-09-02 13:46:29 +0300

    add photo assets to src folder and styles for the hero section and a logo for the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@ export default function Header() {
     return (
         <header>
             <nav>
+                <img src="/src/assets/image__3_-removebg-preview.png" alt="Ken's logo" />
+                <div className="nav-links">
                 <a href="#home">Home</a>
                 <a href="#work">My Work</a>
                 <a href="#contact">Contacts</a>
+                </div>
             </nav>
         </header>
     )
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+    return (
+        <div className="MainContainer">
+          <div className="text">
+            <h1>Hello there, I'm Ken <br />A Front-End Developer</h1>
+            <p>I create websites that look good and work even better.</p>
+            <p>Turning ideas into clean, interactive websites ✨.</p>
+          </div>
+          <div className="wrapper">
+            <img src="/src/assets/ken's image.jpg" alt="Ken's Photo" />
+          </div>
+        </div>
+    )
+}
